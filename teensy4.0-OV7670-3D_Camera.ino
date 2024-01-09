f52053c44bd0dccf0c76ea4bcd297f12c843a608
jjh9397 <jasonhuang2023@u.northwestern.edu>
2024-01-08 22:22:37 -0600

    test gpio read and bit math

--- teensy4.0-OV7670-3D_Camera.ino
+++ teensy4.0-OV7670-3D_Camera.ino
@@ -5,45 +5,61 @@
 
 /***** Definitions *****/
 #define OV7670_I2C_ADDR 0x42 >> 1
+#define DATAPORT GPIO7_PSR  // using GPIO port 2/7 - 7 is the high-speed version
 
 /* Registers */
 #define REG_PID 0x0a  // Product ID MSB
 #define REG_VER 0x0b  // Product ID LSB
 
 /***** Globals *****/
+const uint8_t DATA_PINS[8] = { 10, 12, 11, 13, 6, 9, 8, 7 };  // port 2 pins, arranged for GPIO to be as continuous as possible
+const uint8_t MCLK = 5;
+const uint8_t HS = 4;
+const uint8_t PCLK = 3;
+const uint8_t VC = 2;
 
 /***** Functions *****/
 
 void setup() {
   // Ignore NACK (camera uses SCCB)
   IMXRT_LPI2C1.MCFGR1 |= LPI2C_MCFGR1_IGNACK;
-  pinMode(LED_BUILTIN, OUTPUT);
 
   // Start XCLK signal
-  analogWriteFrequency(9, 16000000);
-  analogWrite(9, 128);
+  analogWriteFrequency(5, 16000000);
+  analogWrite(5, 128);
 
   Wire.begin();
   Serial.begin(9600);
+  Serial.println("########################################");
 
-  if (test_read()) {
-    for (int i = 0; i < 5; i++) {
-      digitalWrite(LED_BUILTIN, HIGH);
-      delay(100);
-      digitalWrite(LED_BUILTIN, LOW);
-      delay(100);
-    }
+  test_i2c_read();
+
+  // set data pins as inputs
+  for (int pin : DATA_PINS) {
+    pinMode(pin, INPUT);
   }
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+  int read = DATAPORT;
+  int r0;
+  int r45;
+  int r67;
+
+  // rearranging bits to be in order
+  // 0,1,2,3 OK
+  // 4,5 -> 2.10, 2.11
+  // 6,7 -> 2.16, 2.17
+  r0 = read & 0b1111;
+  r45 = ((read >> 10) & 0b11) << 4;
+  r67 = ((read >> 16) & 0b11) << 6;
+  read = r0 | r45 | r67;
+  Serial.println(read, BIN);
+
+  delay(100);
 }
 
-bool test_read() {
+bool test_i2c_read() {
   uint8_t pid;
   uint8_t ret;
 
@@ -51,7 +67,7 @@ bool test_read() {
   Wire.write(REG_PID);
   ret = Wire.endTransmission();
   if (ret != 0) {
-    Serial.printf("ERROR: Wire write failed, error %d", ret);
+    Serial.printf("ERROR: Wire write failed, error %d\n", ret);
     return false;
   }
 
