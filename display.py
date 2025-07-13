45b428b55efe6161b3c51dd30f944995dd3fb438
Devin Liu <liudev777@gmail.com>
2025-07-12 21:37:31 -0500

    implemented snapshot

--- display.py
+++ display.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 import cv2
 import serial
@@ -11,10 +12,20 @@ ser = serial.Serial(SERIAL_NUM, 6000000)
 
 while (True):
     data = ser.read_until(b"FRAME")
-    data = ser.read(153600)
+    flag = ser.read(1)          # read shutter trigger
+    data = ser.read(153600)     # read image bytes
     img = cv2.cvtColor(
         np.frombuffer(data, dtype=np.uint8).reshape(240, 320, 2), cv2.COLOR_YUV2BGR_UYVY 
     )[:,:,[2,0,1]]
+    
+    # save image to output folder
+    if flag == b'\x01':
+        timestamp = time.strftime("%Y%m%d_%H%M%S")
+        filename = f"output/snapshot_{timestamp}.png"
+        cv2.imwrite(filename, img)
+        print(f"Screenshot saved: {filename}")
 
     cv2.imwrite("output/test.png", img)
+    print(flag)
     print("WRITE")
+    
